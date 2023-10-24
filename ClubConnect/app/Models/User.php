ed087cb80396f2ff5f38f3b579cd6e3261558b01
MoinGojo <147326781+MoinGojo@users.noreply.github.com>
2023-10-24 20:08:14 +0600

    add theme1

--- ClubConnect/app/Models/User.php
+++ ClubConnect/app/Models/User.php
@@ -26,10 +26,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'phone',
-        'address',
         'password',
-        'usertype',
     ];
 
     /**
